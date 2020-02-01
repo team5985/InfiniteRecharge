f05217af1bee88ed5199c3ca31479d3b043d46bf
LordBBQ <joshua@moranfamily.id.au>
2020-02-01 12:01:24 +1100

    Update DriverControls.java

--- InfiniteRecharge/src/main/java/frc/robot/DriverControls.java
+++ InfiniteRecharge/src/main/java/frc/robot/DriverControls.java
@@ -7,27 +7,70 @@
 
 package frc.robot;
 
-<<<<<<< Updated upstream
-=======
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
->>>>>>> Stashed changes
 /**
  * Add your docs here.
  */
 public class DriverControls {
-<<<<<<< Updated upstream
-=======
 
     Joystick stick;
     XboxController xbox;
+    public static DriverControls mDriverControlsInstance;
+
+    public static DriverControls getDriverControlsInstance() {
+        if (mDriverControlsInstance == null) {
+            mDriverControlsInstance  = new DriverControls();
+        }
+        return mDriverControlsInstance;
+    }
 
     
 
     public DriverControls() {
          stick = new Joystick(Constants.kJoystickPort);
          xbox = new XboxController(Constants.kXboxPort);
+
+         SmartDashboard.setDefaultNumber("Power Gain", 2.0);
+         SmartDashboard.setDefaultNumber("Steering Gain", 2.0);
     }
->>>>>>> Stashed changes
+    /**
+     * Returns true if the joystick has been jerked beyond 0.7.
+     * @return Driver interrupt command.
+     */
+    public boolean getStickInterupt() {
+
+		if(stick.getX() >= 0.7) {
+			end = true;
+		} else if(stick.getX() <= -0.7) {
+			end = true;
+		} else if(stick.getY() >= 0.7) {
+			end = true;
+		} else if(stick.getY() <= -0.7) {
+			end = true;
+		} else if(stick.getZ() >= 0.7) {
+			end = true;
+		} else if(stick.getZ() <= -0.7) {
+			end = true;
+		} else if(xBox.getX(Hand.kLeft) < -0.7){
+			end = true;
+		} else if(xBox.getX(Hand.kLeft) > 0.7){
+			end = true;
+		} else if(xBox.getX(Hand.kRight) < -0.7){
+			end = true;
+		} else if(xBox.getX(Hand.kRight) > 0.7){
+			end = true;
+		} else if(xBox.getY(Hand.kLeft) < -0.7){
+			end = true;
+		} else if(xBox.getY(Hand.kLeft) > 0.7){
+			end = true;
+		} else if(xBox.getY(Hand.kRight) < -0.7){
+			end = true;
+		} else if(xBox.getY(Hand.kRight) > 0.7){
+			end = true;
+		}
+	    return end;
+	}
 }
